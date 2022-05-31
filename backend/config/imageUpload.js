214ccf32b09e89a1b775f3a042b8a36060f9d879
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-05-31 12:38:08 +0200

    fix url

--- backend/config/imageUpload.js
+++ backend/config/imageUpload.js
@@ -13,7 +13,10 @@ const imageUpload = multer({
     },
   }),
   fileFilter: (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    if (file.mimetype === 'image/png' 
+    || file.mimetype === 'image/jpg' 
+    || file.mimetype === 'image/jpeg')
+     {
       cb(null, true);
     } else {
       cb(null, false);
--- src/config.js
+++ src/config.js
@@ -1,8 +1,2 @@
 /* eslint-disable linebreak-style */
-export const IMAGES_URL = (process.env.NODE_ENV === 'production') ? '/uploads' : 'http://localhost:8000/uploads';
-export const API_URL = (process.env.NODE_ENV === 'production') ? '/api' : 'http://localhost:8000/api';
-
-export const AUTH_URL =
-  process.env.NODE_ENV === 'production'
-    ? '/auth'
-    : 'http://localhost:8000/auth';
+export const API_URL = process.env.NODE_ENV === 'production' ? 'api' : 'http://localhost:8000/api';
