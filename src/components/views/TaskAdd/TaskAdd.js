69631134635f5a51f5462af8fc6fe9e319c28a2c
Justyna KUJAWA <justy.kujawa@gmail.com>
2023-05-24 10:09:32 +0200

    first commit

--- src/components/views/TaskAdd/TaskAdd.js
+++ src/components/views/TaskAdd/TaskAdd.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { addTaskRequest } from "../../../redux/postsRedux";
+import { addTaskRequest } from "../../../redux/tasksRedux";
 import TaskForm from "../../features/TaskForm/TaskForm";
 import styles from "./TaskAdd.module.scss";
 
